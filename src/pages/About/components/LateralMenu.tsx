288d8cd761c881ab5298264c716c146f543f6952
bruno.mileto <brunomileto@outlook.com>
2022-10-01 13:51:29 -0300

    change spaces on lateral menus

--- src/pages/About/components/LateralMenu.tsx
+++ src/pages/About/components/LateralMenu.tsx
@@ -1,4 +1,4 @@
-import { DisclosureMenu } from "./DisclosureMenu";
+import { DisclosureMenu } from './DisclosureMenu';
 
 interface Props {
   handleSelectedMenu: (name: string) => void;
@@ -8,7 +8,7 @@ export function LateralMenu({ handleSelectedMenu }: Props) {
   return (
     <div
       id="aboutDisclosure"
-      className="flex flex-col gap-1 md:gap-0 relative md:w-[calc(40000vw/1920+1px)] 
+      className="flex flex-col gap-2 md:gap-0 relative md:w-[calc(40000vw/1920+1px)] 
                 lg:w-[calc(31100vw/1920)] md:border-r-1 md:border-lines 
                 md:flex-shrink-0 md:overflow-x-hidden"
     >
--- src/pages/Contact/components/LateralMenu.tsx
+++ src/pages/Contact/components/LateralMenu.tsx
@@ -1,10 +1,10 @@
-import { DisclosureMenu } from "./DisclosureMenu";
+import { DisclosureMenu } from './DisclosureMenu';
 
 export function LateralMenu() {
   return (
     <div
       id="aboutDisclosure"
-      className="flex flex-col gap-1 md:gap-0 relative md:w-[calc(40000vw/1920+1px)] 
+      className="flex flex-col gap-2 md:gap-0 relative md:w-[calc(40000vw/1920+1px)] 
                 lg:w-[calc(31100vw/1920)] md:border-r-1 md:border-lines 
                 md:flex-shrink-0 md:overflow-x-hidden"
     >
