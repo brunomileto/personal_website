50e216f00ecb606eb61cf2e46bd68c4961971018
bruno.mileto <brunomileto@outlook.com>
2022-12-28 18:56:08 -0300

    add queries and add locale variables to queries

--- src/libs/apollo/apolloQueries.ts
+++ src/libs/apollo/apolloQueries.ts
@@ -1,8 +1,14 @@
 import { gql } from '@apollo/client';
 
+export enum Language {
+  ptBR = "pt_BR",
+  enUS = "en",
+}
+
 export const GET_ALL_PROJECTS_SIMPLE_QUERY = gql`
-  query AllProjectsShort {
-    projects {
+  # query AllProjectsShort($locales: [Locale!] = [language]) {
+  query AllProjectsShort($locales: [Locale!]!) {
+    projects(locales: $locales) {
       id
       shortname
       shortDescription
@@ -11,6 +17,36 @@ export const GET_ALL_PROJECTS_SIMPLE_QUERY = gql`
       projectTypes
       coverPicture {
         url
+      }
+    }
+  }
+`;
+
+export const GET_SITE_PAGE_NAMES_QUERY = gql`
+  # query PageNames($locales: [Locale!] = [language]) {
+  query Pages($locales: [Locale!]!) {
+    pages(locales: $locales) {
+      name
+      path
+      locale
+    }
+  }
+`;
+
+export const GET_ABOUT_MENU = gql`
+  query AboutMenus($locales: [Locale!]!) {
+    aboutMenus(locales: $locales) {
+      name
+      aboutMenuType
+      aboutSubMenus {
+        name
+        aboutSubMenuType
+        aboutSubMenuItems {
+          name
+          description {
+            text
+          }
+        }
       }
     }
   }
