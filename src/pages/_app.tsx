ab54a8b50fd05ca37c66a6dcc5bd6a04f1dece5a
bruno.mileto <brunomileto@outlook.com>
2022-12-28 18:56:58 -0300

    changes to add locale on graphql queries and new queries

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,27 @@
 import '../styles/globals.css';
 
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
 import { ApolloProvider } from '@apollo/client';
 
 import { CodesContextProvider } from '../context/CodesContext';
+import { PageNamesContextProvider } from '../context/PageNamesContext';
 import { BaseLayout } from '../layouts/BaseLayout';
 import { apolloClient } from '../libs/apollo/apollo';
 
 import type { AppProps } from "next/app";
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const locale = router.locale;
   return (
     <ApolloProvider client={apolloClient}>
       <CodesContextProvider>
-        <BaseLayout>
-          <Component {...pageProps} />
-        </BaseLayout>
+        <PageNamesContextProvider language={locale}>
+          <BaseLayout>
+            <Component {...pageProps} />
+          </BaseLayout>
+        </PageNamesContextProvider>
       </CodesContextProvider>
     </ApolloProvider>
   );
